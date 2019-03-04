f7505586000846bf549962832935caa76406c5c3
PaulMaxAvalosAguilar <pauldromeasaurio@hotmail.com>
2019-03-03 18:03:45 -0600

    Moved test functions to a signle file

--- Mechatronics/NONEABISoftware/Test/Src_Modules/MicroC_Application/src/serialLoopback.c
+++ Mechatronics/NONEABISoftware/Test/Src_Modules/MicroC_Application/src/serialLoopback.c
@@ -1,13 +1,28 @@
-/*
-A simple test of serial-port functionality.
-Takes in a character at a time and sends it right back out,
- displaying the ASCII value on the LEDs.
-*/
-
-// ------- Preamble -------- //
 #include <avr/io.h>
 #include <util/delay.h>
-#include "USART.h"
+#include <util/setbaud.h>
+
+void initUSART(void) {                         
+  UBRR0H = UBRRH_VALUE;                        
+  UBRR0L = UBRRL_VALUE;
+#if USE_2X
+  UCSR0A |= (1 << U2X0);
+#else
+  UCSR0A &= ~(1 << U2X0);
+#endif
+
+  UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);   
+}
+
+void printString(const char myString[]) {
+  uint8_t i = 0;
+  while (myString[i]) {
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    UDR0 = myString[i];                  
+    i++;
+  }
+}
 
 int main(void) {
 
@@ -19,6 +34,8 @@ int main(void) {
   while (1) {
 
 
-  }                                                  /* End event loop */
+  }
+  /* End event loop */
+  
   return 0;
 }
